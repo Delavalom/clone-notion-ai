d4388047361daefe0a25b06dbbc42f49aa072975
Delavalom <laarvelo@icloud.com>
2023-02-19 23:03:56 -0400

    Update importing from components, more cleaner

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,17 +1,1 @@
-import { ReactNode, type FC } from "react";
-import { Sidebar } from "../Sidebar";
-import { OverlayBg } from "./OverlayBg";
-
-type Props = {
-  children: ReactNode;
-};
-
-export const AppLayout: FC<Props> = ({ children }) => {
-  return (
-    <main className="bg-white w-screen h-screen flex">
-      <Sidebar notes={[{title: "Cooking Recipes"}, {title: "Employees"}, {title: "Blog Posts"}]} />
-      {/* <OverlayBg /> */}
-      <section className="flex-1">{children}</section>
-    </main>
-  );
-};
+export { AppLayout  } from './AppLayout'
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,40 @@
+import { type FC } from "react";
+
+type Notes = {
+    title: string
+    body?: any
+}
+
+type Props = {
+    notes: Notes[]
+};
+
+export const Sidebar: FC<Props> = ({}) => {
+  return (
+    <aside className=" bg-zinc-100 w-3/4 md:w-2/4 lg:w-1/4 2xl:w-2/12 h-full flex flex-col border-r-2 border-opacity-100">
+      <section id="AccountBar" className="p-4 w-fit hover:bg-zinc-200">
+        <p className="w-60">Luis Arvelo&apos;s Notion</p>
+      </section>
+      <section id="NotesContainer" className="h-full flex flex-col justify-between">
+        <ul id="Notes" className="flex flex-col gap-2 px-1 py-4">
+          <li className="hover:bg-zinc-200 rounded-md py-1 px-2 text-zinc-500 text-md">
+            Cooking Recipes
+          </li>
+          <li className="hover:bg-zinc-200 rounded-md py-1 px-2 text-zinc-500 text-md">
+            Employees
+          </li>
+          <li className="hover:bg-zinc-200 rounded-md py-1 px-2 text-zinc-500 text-md">
+            Blog Posts
+          </li>
+        </ul>
+        <button
+          type="button"
+          id="NewNoteButton"
+          className=" hover:bg-zinc-200 py-4 px-4 border-y-2 text-start flex items-center gap-2 text-zinc-500 text-md"
+        >
+          + New Page
+        </button>
+      </section>
+    </aside>
+  );
+};
