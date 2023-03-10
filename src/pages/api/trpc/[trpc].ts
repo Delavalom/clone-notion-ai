b966666fb61359e2eac7e24ffb250856fe430a47
Delavalom <laarvelo@icloud.com>
2023-03-09 22:03:54 -0400

    adding prisma and missing trpc set up

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,10 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { appRouter } from '../../../server/routers/_app';
+import {createTRPCContext} from '../../../server/context'
+
 // export API handler
 // @see https://trpc.io/docs/api-handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => ({}),
+  createContext: createTRPCContext,
 });
