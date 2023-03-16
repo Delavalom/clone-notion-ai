56ad231d9b8dc127a1d34038f0823479230f92fc
Delavalom <laarvelo@icloud.com>
2023-03-16 12:34:36 -0400

    updated schema to support github provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,3 +1,4 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
@@ -5,6 +6,7 @@ import {
   type DefaultSession,
 } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { prisma } from "./db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -17,7 +19,7 @@ declare module "next-auth" {
     user: {
       id: string;
       // ...other properties
-      name: string;
+      username: string;
     } & DefaultSession["user"];
   }
 
@@ -33,18 +35,6 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
-    },
-    async signIn(params) {
-      return `/setup`
-    },
-  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
@@ -58,8 +48,21 @@ export const authOptions: NextAuthOptions = {
      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
      *
      * @see https://next-auth.js.org/providers/github
-     */
+    */
   ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+        // session.user.role = user.role; <-- put other properties on the session here
+      }
+      return session;
+    },
+    async signIn() {
+      return '/setup'
+    }
+  },
 };
 
 /**
