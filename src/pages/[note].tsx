9126f29bf7803ed8745fb8e51e94866ac7c95639
Delavalom <laarvelo@icloud.com>
2023-03-26 01:50:32 -0400

    update title debouncer

--- src/pages/[note].tsx
+++ src/pages/[note].tsx
@@ -1,34 +1,46 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { type FC } from "react";
+import { ChangeEvent, useState, type FC, useEffect } from "react";
 import { OverlayBg } from "~/components/Layouts/OverlayBg";
 import { Sidebar } from "~/components/Sidebar";
 import { NavigationProvider } from "~/context/NavigationContext";
 import { api } from "~/utils/api";
-import notesBg from '../../public/notesBg.png'
+import notesBg from "../../public/notesBg.png";
 
 type Props = {};
 
 const Note: FC<Props> = ({}) => {
   const { data: session, status } = useSession();
   const { data: notes, isLoading } = api.note.getNotes.useQuery();
-  const { note } = useRouter().query;
+  const { note: path } = useRouter().query;
+  const { mutate } = api.note.updateNoteTitle.useMutation();
+  const note = notes?.find((note) => note.id === path);
+  const [input, setInput] = useState<string>(note?.title ?? "untitled");
+
+  useEffect(() => {
+    if (input === note?.title || !note?.id) return;
+
+    const updateTitle = setTimeout(() => {
+      mutate({ id: note.id, title: input });
+    }, 300);
+
+    return () => clearTimeout(updateTitle);
+  }, [input]);
 
   if (isLoading || status === "loading") {
     return <NoteSqueleton></NoteSqueleton>;
   }
-
   return (
     <NavigationProvider>
       <main className="flex h-screen w-screen bg-white">
         <Sidebar session={session} notes={notes ?? []} />
         <OverlayBg />
         <section className="flex h-full w-full flex-col items-center overflow-y-scroll bg-white">
-          <picture className="w-[1440px] h-[270px]">
+          <picture className="h-[270px] w-full">
             <Image
               src={notesBg}
-              width={1440}
+              width={1640}
               height={270}
               alt={`This is the background for the note`}
             />
@@ -36,16 +48,20 @@ const Note: FC<Props> = ({}) => {
           <div className="mx-auto flex h-full w-full max-w-[900px] flex-col items-center">
             <section
               id="titleSection"
-              className="flex h-full max-h-32 w-full flex-col items-center justify-end"
+              className="mb-4 flex h-full max-h-32 w-full flex-col items-center justify-end"
             >
-              {note}
+              <input
+                onChange={(e) => setInput(e.currentTarget.value)}
+                value={input}
+                className="w-full max-w-[700px] text-left text-4xl font-black text-gray-800 outline-none"
+              />
             </section>
             <article
               role="textbox"
               aria-multiline={true}
-              className="flex h-full w-full flex-1 flex-col items-center"
+              className="flex h-full w-full max-w-[700px] flex-1 flex-col text-left"
             >
-              {note}
+              {path}
             </article>
           </div>
         </section>
--- src/server/routers/note.ts
+++ src/server/routers/note.ts
@@ -1,19 +1,42 @@
 import { protectedProcedure, router } from "../trpc";
 import { getLastUpdatedNote } from "../helpers/getLastUpdatedNote";
+import { z } from "zod";
 
 export const noteRouter = router({
-  getNotes: protectedProcedure.query(async ({ctx}) => {
-    return ctx.prisma.note.findMany()
+  getNotes: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.prisma.note.findMany();
   }),
-  createNote: protectedProcedure.mutation(async ({ctx}) => {
+  createNote: protectedProcedure.mutation(async ({ ctx }) => {
     const newNote = await ctx.prisma.note.create({
       data: {
         userId: ctx.session.user.id,
-        body: ""
-      }
-    })
-    return newNote.id
+        body: "",
+      },
+    });
+    return newNote.id;
   }),
+  updateNoteTitle: protectedProcedure
+    .input(z.object({id: z.string(), title: z.string()}))
+    .mutation(({ ctx, input }) => {
+      if (input.title === "") {
+        return ctx.prisma.note.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            title: "untitled",
+          },
+        });
+      }
+      return ctx.prisma.note.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+        },
+      });
+    }),
   getLastUpdatedNote: protectedProcedure.query(async ({ ctx }) => {
     return getLastUpdatedNote(ctx.session.user.id);
   }),
