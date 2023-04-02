bee34b1bbd1766c8c7d5c9c1d4b3ded5951e62ca
Delavalom <laarvelo@icloud.com>
2023-04-02 03:18:13 -0400

    build fix

--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -1,11 +1,11 @@
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import type { Session } from "next-auth";
 import superjson from 'superjson';
-import { prisma } from "../db";
+import { prisma, redis } from "../db";
 import { appRouter } from "../routers/_app";
 
 export const ssgHelper = (session:  Session | null) => createProxySSGHelpers({
   router: appRouter,
-  ctx: { prisma, session},
+  ctx: { prisma, session, redis},
   transformer: superjson
 });
